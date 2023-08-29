6caed67a8d3d66699ffc358740343424de713021
Martin Dieblich <martin.dieblich@gmx.de>
2023-08-29 19:04:07 +0200

    initial commit, empty project

---
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/SchiedsrichterAssistentApplication.java
@@ -0,0 +1,13 @@
+package com.dieblich.handball.schiedsrichterassistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchiedsrichterAssistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchiedsrichterAssistentApplication.class, args);
+	}
+
+}
---
+++ schiedsrichter-assistent/src/test/java/com/dieblich/handball/schiedsrichterassistent/SchiedsrichterAssistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dieblich.handball.schiedsrichterassistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchiedsrichterAssistentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
