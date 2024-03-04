d784725bbc4c2caba12fc121cd6dcc9b57775672
Martin Dieblich <martin.dieblich@gmx.de>
2024-03-04 23:46:16 +0100

    getContent from more complex Emails

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/Email.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/Email.java
@@ -3,6 +3,7 @@ package com.dieblich.handball.schiedsrichterassistent.mail;
 import jakarta.mail.*;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -40,7 +41,39 @@ public class Email {
     }
 
     public String getContent() throws MessagingException, IOException {
-        return message.getContent().toString();
+        if (message.isMimeType("text/plain")) {
+            return message.getContent().toString();
+        }
+        if (message.isMimeType("multipart/*")) {
+            MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+            return getTextFromMimeMultipart(mimeMultipart);
+        }
+        return "";
+    }
+
+    private String getTextFromMimeMultipart(MimeMultipart mimeMultipart)  throws MessagingException, IOException{
+        String result = "";
+        for (int i = 0; i < mimeMultipart.getCount(); i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                return result + "\n" + bodyPart.getContent(); // without return, same text appears twice in my tests
+            }
+            result += this.parseBodyPart(bodyPart);
+        }
+        return result;
+    }
+
+    private String parseBodyPart(BodyPart bodyPart) throws MessagingException, IOException {
+        if (bodyPart.isMimeType("text/html")) {
+            return "\n" + org.jsoup.Jsoup
+                    .parse(bodyPart.getContent().toString())
+                    .text();
+        }
+        if (bodyPart.getContent() instanceof MimeMultipart){
+            return getTextFromMimeMultipart((MimeMultipart)bodyPart.getContent());
+        }
+
+        return "";
     }
 
     public void deleteImmediately() throws MessagingException {
@@ -70,8 +103,12 @@ public class Email {
     public Optional<String> getFrom() throws MessagingException {
         return getAllSenders().stream().findAny();
     }
+    public String getSubject() throws MessagingException {
+        return message.getSubject();
+    }
 
     public void send() throws MessagingException {
         Transport.send(message);
     }
+
 }
