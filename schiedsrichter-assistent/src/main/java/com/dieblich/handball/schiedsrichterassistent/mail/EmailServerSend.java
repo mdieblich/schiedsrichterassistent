a79a4c1a5fbda40da14996cba3cc77d56ac700fd
Martin Dieblich <martin.dieblich@gmx.de>
2024-03-24 20:37:55 +0100

    Renamed variables

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/EmailServerSend.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/EmailServerSend.java
@@ -8,10 +8,10 @@ import java.util.Properties;
 public class EmailServerSend {
 
     private final Session session;
-    private final String user;
+    private final String botEmailAddress;
 
-    public EmailServerSend(String host, int port, String user, String password) {
-        this.user = user;
+    public EmailServerSend(String host, int port, String botEmailAddress, String password) {
+        this.botEmailAddress = botEmailAddress;
         Properties props = System.getProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
@@ -19,13 +19,13 @@ public class EmailServerSend {
         props.put("mail.smtp.port", port);
         Authenticator authenticator = new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(user, password);
+                return new PasswordAuthentication(botEmailAddress, password);
             }
         };
         session = Session.getInstance(props, authenticator);
     }
 
-    public WelcomeEmail createWelcomeEmail(String receiver) throws MessagingException {
-        return new WelcomeEmail(user, receiver, session);
+    public WelcomeEmail createWelcomeEmail(String schiriEmailAddress) throws MessagingException {
+        return new WelcomeEmail(botEmailAddress, schiriEmailAddress, session);
     }
 }
