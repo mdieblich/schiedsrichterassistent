2cc16d85caf9bd914dc79352cfb611a81fa6b836
Martin Dieblich <martin.dieblich@gmx.de>
2024-03-24 20:32:12 +0100

    delete all Emails at the end

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/MailController.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/MailController.java
@@ -96,7 +96,7 @@ public class MailController {
                 askForRegistration(unknownSender);
             }
         } finally {
-            //inbox.deleteAll();
+            inbox.deleteAll();
         }
     }
 
