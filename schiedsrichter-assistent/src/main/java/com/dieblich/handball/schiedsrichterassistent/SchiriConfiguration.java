841af58b9faa3734d7da404f68a01a6d1a1fa635
Martin Dieblich <martin.dieblich@gmx.de>
2024-03-27 00:41:42 +0100

    moved Default to factoryMethod implement updateWith

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/SchiriConfiguration.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/SchiriConfiguration.java
@@ -1,38 +1,45 @@
 package com.dieblich.handball.schiedsrichterassistent;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @ToString
 @EqualsAndHashCode
+@NoArgsConstructor
 @SuppressWarnings({"NonAsciiCharacters", "SpellCheckingInspection"})
 public class SchiriConfiguration {
-    public Benutzerdaten Benutzerdaten;
+    public Benutzerdaten Benutzerdaten = new Benutzerdaten();
     public Spielablauf Spielablauf = new Spielablauf();
 
-    public SchiriConfiguration(@JsonProperty("Benutzerdaten.Email") String email){
+    public SchiriConfiguration(String email){
         Benutzerdaten = new Benutzerdaten(email);
     }
 
     @ToString
     @EqualsAndHashCode
+    @NoArgsConstructor
     public static class Benutzerdaten {
         public String Email;
-        public String Vorname = "Max";
-        public String Nachname = "Mustermann";
-        public String Adresse = "Musterstr. 17, 54321 Köln";
+        public String Vorname;
+        public String Nachname;
+        public String Adresse;
         @JsonInclude(JsonInclude.Include.NON_NULL)
         public Double Längengrad;
         @JsonInclude(JsonInclude.Include.NON_NULL)
         public Double Breitengrad;
-        public Benutzerdaten(@JsonProperty("Email") String email){
+        public Benutzerdaten(String email){
             this.Email = email;
         }
     }
@@ -40,21 +47,91 @@ public class SchiriConfiguration {
     @ToString
     @EqualsAndHashCode
     public static class Spielablauf{
-        public int UmziehenVorSpiel = 15;
-        public int PapierKramNachSpiel = 15;
-        public int UmziehenNachSpiel = 15;
+        public Integer UmziehenVorSpiel;
+        public Integer PapierKramNachSpiel;
+        public Integer UmziehenNachSpiel;
         public TechnischeBesprechung TechnischeBesprechung = new TechnischeBesprechung();
 
         @ToString
         @EqualsAndHashCode
         public static class TechnischeBesprechung{
-            public int StandardDauerInMinuten = 30;
-            public Map<String, Integer> Abweichungen = Map.of("Regionalliga", 45, "Oberliga", 45);
+            public Integer StandardDauerInMinuten;
+            public Map<String, Integer> Abweichungen = new HashMap<>();
+        }
+    }
+
+    public static SchiriConfiguration NEW_DEFAULT(String email){
+        SchiriConfiguration config = new SchiriConfiguration();
+        config.Benutzerdaten.Email = email;
+        config.Benutzerdaten.Vorname = "Max";
+        config.Benutzerdaten.Nachname = "Mustermann";
+        config.Benutzerdaten.Adresse = "Musterstr. 17, 54321 Köln";
+        config.Spielablauf.UmziehenVorSpiel = 15;
+        config.Spielablauf.PapierKramNachSpiel = 15;
+        config.Spielablauf.UmziehenNachSpiel = 15;
+        config.Spielablauf.TechnischeBesprechung.StandardDauerInMinuten = 30;
+        config.Spielablauf.TechnischeBesprechung.Abweichungen.putAll(Map.of(
+                // put Map.of inside of HashMap to have it mutable
+                "Regionalliga", 45,
+                "Oberliga", 45
+        ));
+        return config;
+    }
+    public void updateWith(String configUpdate, Function<String, Optional<double[]>> addressToGeoLocation, Consumer<String> log) {
+        try {
+        SchiriConfiguration newConfig = mapper.readValue(configUpdate, SchiriConfiguration.class);
+        if(newConfig.Benutzerdaten != null){
+            if(newConfig.Benutzerdaten.Email != null) {Benutzerdaten.Email = newConfig.Benutzerdaten.Email;}
+            if(newConfig.Benutzerdaten.Vorname != null) {Benutzerdaten.Vorname = newConfig.Benutzerdaten.Vorname;}
+            if(newConfig.Benutzerdaten.Nachname != null) {Benutzerdaten.Nachname = newConfig.Benutzerdaten.Nachname;}
+
+            if(newConfig.Benutzerdaten.Adresse != null) {
+                if(newConfig.Benutzerdaten.Längengrad != null && newConfig.Benutzerdaten.Breitengrad != null){
+                    // all 3 have been set
+                    Benutzerdaten.Adresse = newConfig.Benutzerdaten.Adresse;
+                    Benutzerdaten.Längengrad = newConfig.Benutzerdaten.Längengrad;
+                    Benutzerdaten.Breitengrad = newConfig.Benutzerdaten.Breitengrad;
+                } else {
+                    // only address was set, let's search the geolocation
+                    Optional<double[]> optionalGeoLocation = addressToGeoLocation.apply(newConfig.Benutzerdaten.Adresse);
+                    if(optionalGeoLocation.isPresent()){
+                        Benutzerdaten.Adresse = newConfig.Benutzerdaten.Adresse;
+                        Benutzerdaten.Längengrad = optionalGeoLocation.get()[0];
+                        Benutzerdaten.Breitengrad = optionalGeoLocation.get()[1];
+                    }else{
+                        log.accept("Für die Adresse \""+newConfig.Benutzerdaten.Adresse+"\" konnten Längen- und Breitengrad nicht bestimmt werden. Sie wird daher nicht übernomen.");
+                        log.accept("FALLS DAS PROBLEM WIEDERHOLT AUFTRITT SO KANNST DU FOLGENDES TUN:");
+                        log.accept("1. Bestimme mithilfe eines Kartendienstes (z.B. https://www.gpskoordinaten.de/) deinen Längen- und Breitengrad.");
+                        log.accept("2. Setze in der Konfiguration im Abschnitt \"Benutzerdaten\" die Adresse UND Werte für \"Löngengrad\"");
+                        log.accept("   und \"Breitengrad\". Beachte bitte, dass du min. 4-Nachkommastellen verwendest.");
+                        log.accept("   Verwendet wird das Koordinatensystem WGS 84");
+                    }
+                }
+            }
+        }
+        if(newConfig.Spielablauf != null){
+            if(newConfig.Spielablauf.UmziehenVorSpiel != null) {Spielablauf.UmziehenVorSpiel = newConfig.Spielablauf.UmziehenVorSpiel;}
+            if(newConfig.Spielablauf.PapierKramNachSpiel != null) {Spielablauf.PapierKramNachSpiel = newConfig.Spielablauf.PapierKramNachSpiel;}
+            if(newConfig.Spielablauf.UmziehenNachSpiel != null) {Spielablauf.UmziehenNachSpiel = newConfig.Spielablauf.UmziehenNachSpiel;}
+            if(newConfig.Spielablauf.TechnischeBesprechung != null) {
+                if(newConfig.Spielablauf.TechnischeBesprechung.StandardDauerInMinuten != null) {
+                    Spielablauf.TechnischeBesprechung.StandardDauerInMinuten = newConfig.Spielablauf.TechnischeBesprechung.StandardDauerInMinuten;
+                }
+                if(newConfig.Spielablauf.TechnischeBesprechung.Abweichungen != null){
+                    Spielablauf.TechnischeBesprechung.Abweichungen.putAll(newConfig.Spielablauf.TechnischeBesprechung.Abweichungen);
+                }
+            }
+        }
+
+        } catch (JsonProcessingException e) {
+            log.accept("Fehler beim Lesen der Konfiguration: " + e.getMessage());
         }
     }
 
     /// JSON serialization and deserialization
-    private final static ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    private final static ObjectMapper mapper = new ObjectMapper()
+            .enable(SerializationFeature.INDENT_OUTPUT)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     public String toJSON() throws JsonProcessingException {
         return mapper.writeValueAsString(this);
--- schiedsrichter-assistent/src/test/java/com/dieblich/handball/schiedsrichterassistent/SchiriConfigurationTest.java
+++ schiedsrichter-assistent/src/test/java/com/dieblich/handball/schiedsrichterassistent/SchiriConfigurationTest.java
@@ -5,13 +5,20 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SchiriConfigurationTest {
 
     @Test
     public void hasNiceJSON() throws JsonProcessingException {
-        SchiriConfiguration config = new SchiriConfiguration("muster@max.de");
+        SchiriConfiguration config = SchiriConfiguration.NEW_DEFAULT("muster@max.de");
         config.Benutzerdaten.Längengrad  = 1.23456;
         config.Benutzerdaten.Breitengrad = 5.67891;
 
@@ -48,7 +55,7 @@ class SchiriConfigurationTest {
     }
     @Test
     public void jsonLeavesOutLongitudeAndLatitude() throws JsonProcessingException {
-        SchiriConfiguration config = new SchiriConfiguration("muster@max.de");
+        SchiriConfiguration config = SchiriConfiguration.NEW_DEFAULT("muster@max.de");
         config.Benutzerdaten.Längengrad  = null;
         config.Benutzerdaten.Breitengrad = null;
 
@@ -111,10 +118,238 @@ class SchiriConfigurationTest {
 
         SchiriConfiguration actual = SchiriConfiguration.fromJSON(json);
 
-        SchiriConfiguration expected = new SchiriConfiguration("muster@max.de");
+        SchiriConfiguration expected = SchiriConfiguration.NEW_DEFAULT("muster@max.de");
         expected.Benutzerdaten.Längengrad  = 1.23456;
         expected.Benutzerdaten.Breitengrad = 5.67891;
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void updateWithNothingChangesNothing() {
+        SchiriConfiguration config = new SchiriConfiguration("muster@moritz.de");
+        config.Benutzerdaten.Vorname = "Moritz";
+        config.Benutzerdaten.Nachname = "Schulze";
+        config.Benutzerdaten.Adresse = "Irgendo 12, 12345 Berlin";
+        config.Benutzerdaten.Längengrad = 5.0;
+        config.Benutzerdaten.Breitengrad = 6.0;
+        config.Spielablauf.UmziehenVorSpiel = 20;
+        config.Spielablauf.PapierKramNachSpiel = 5;
+        config.Spielablauf.UmziehenNachSpiel = 25;
+        config.Spielablauf.TechnischeBesprechung.StandardDauerInMinuten = 90;
+        config.Spielablauf.TechnischeBesprechung.Abweichungen.putAll(Map.of(
+                "Regionalliga", 100,
+                "Oberliga", 60,
+                "Kreisklasse", 4
+        ));
+
+        config.updateWith("{}", fakeAddressToGeoLocation, fakeLog);
+
+        assertEquals("muster@moritz.de", config.Benutzerdaten.Email);
+        assertEquals("Moritz", config.Benutzerdaten.Vorname);
+        assertEquals("Schulze", config.Benutzerdaten.Nachname);
+        assertEquals("Irgendo 12, 12345 Berlin", config.Benutzerdaten.Adresse);
+        assertEquals(5.0, config.Benutzerdaten.Längengrad);
+        assertEquals(6.0, config.Benutzerdaten.Breitengrad);
+        assertEquals(20, config.Spielablauf.UmziehenVorSpiel);
+        assertEquals(5, config.Spielablauf.PapierKramNachSpiel );
+        assertEquals(25, config.Spielablauf.UmziehenNachSpiel );
+        assertEquals(90, config.Spielablauf.TechnischeBesprechung.StandardDauerInMinuten);
+        assertEquals(Map.of(
+        "Regionalliga", 100,
+        "Oberliga", 60,
+        "Kreisklasse", 4
+        ), config.Spielablauf.TechnischeBesprechung.Abweichungen);
+    }
+    @Test
+    public void updateWithSingleEntry() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        config.updateWith("""
+                {
+                	"Spielablauf": {
+                		"TechnischeBesprechung": {
+                			"StandardDauerInMinuten": 90
+                		}
+                	}
+                }
+                """, fakeAddressToGeoLocation, fakeLog);
+
+        assertEquals(90, config.Spielablauf.TechnischeBesprechung.StandardDauerInMinuten);
+    }
+    @Test
+    public void updateWithMultipleEntries() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        config.updateWith("""
+                {
+                 	"Benutzerdaten": {
+                        "Vorname": "Moritz"
+                 	},
+                	"Spielablauf": {
+                		"TechnischeBesprechung": {
+                			"StandardDauerInMinuten": 90
+                		}
+                	}
+                }
+                """, fakeAddressToGeoLocation, fakeLog);
+
+        assertEquals("Moritz", config.Benutzerdaten.Vorname);
+        assertEquals(90, config.Spielablauf.TechnischeBesprechung.StandardDauerInMinuten);
+    }
+
+    @Test
+    public void updateWithTechnischeBesprechungLiga() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        config.updateWith("""
+                {
+                	"Spielablauf": {
+                		"TechnischeBesprechung": {
+                            "Abweichungen": {
+                                "Regionalliga": 50
+                            }
+                		}
+                	}
+                }
+                """, fakeAddressToGeoLocation, fakeLog);
+
+        assertEquals(50, config.Spielablauf.TechnischeBesprechung.Abweichungen.get("Regionalliga"));
+    }
+    @Test
+    public void updateWithCreatesLiga() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        config.updateWith("""
+                {
+                	"Spielablauf": {
+                		"TechnischeBesprechung": {
+                            "Abweichungen": {
+                                "Kreisklasse": 20
+                            }
+                		}
+                	}
+                }
+                """, fakeAddressToGeoLocation, fakeLog);
+
+        assertEquals(20, config.Spielablauf.TechnischeBesprechung.Abweichungen.get("Kreisklasse"));
+    }
+
+    @Test
+    public void updateWithAdresseCreatesGeoLocation() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        config.updateWith("""
+                {
+                	"Benutzerdaten": {
+                		"Adresse": "Dudelstr. 15, 12345 Dudelstadt"
+                	}
+                }
+                """, String -> Optional.of(new double[]{3,5}), fakeLog);
+
+        assertEquals("Dudelstr. 15, 12345 Dudelstadt", config.Benutzerdaten.Adresse);
+        assertEquals(3, config.Benutzerdaten.Längengrad);
+        assertEquals(5, config.Benutzerdaten.Breitengrad);
+    }
+    @Test
+    public void updateWithUnknwonAdresseChangesNothing() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        String adresseBefore = config.Benutzerdaten.Adresse;
+        config.updateWith("""
+                {
+                	"Benutzerdaten": {
+                		"Adresse": "Dudelstr. 15, 12345 Dudelstadt"
+                	}
+                }
+                """, String -> Optional.empty(), fakeLog);
+
+        assertEquals(adresseBefore, config.Benutzerdaten.Adresse);
+        assertNull(config.Benutzerdaten.Längengrad);
+        assertNull(config.Benutzerdaten.Breitengrad);
+    }
+    @Test
+    public void updateWithUnknwonAdresseCreatesLogEntry() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        List<String> log = new ArrayList<>();
+        config.updateWith("""
+                {
+                	"Benutzerdaten": {
+                		"Adresse": "Dudelstr. 15, 12345 Dudelstadt"
+                	}
+                }
+                """, String -> Optional.empty(), log::add);
+
+        List<String> expectedLog = List.of(
+            "Für die Adresse \"Dudelstr. 15, 12345 Dudelstadt\" konnten Längen- und Breitengrad nicht bestimmt werden. Sie wird daher nicht übernomen.",
+            "FALLS DAS PROBLEM WIEDERHOLT AUFTRITT SO KANNST DU FOLGENDES TUN:",
+            "1. Bestimme mithilfe eines Kartendienstes (z.B. https://www.gpskoordinaten.de/) deinen Längen- und Breitengrad.",
+            "2. Setze in der Konfiguration im Abschnitt \"Benutzerdaten\" die Adresse UND Werte für \"Löngengrad\"",
+            "   und \"Breitengrad\". Beachte bitte, dass du min. 4-Nachkommastellen verwendest.",
+            "   Verwendet wird das Koordinatensystem WGS 84"
+        );
+
+        assertEquals(expectedLog, log);
+    }
+
+    @Test
+    public void updateWithUnknwonAdresseButGeoLocationWorks() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        config.updateWith("""
+                {
+                	"Benutzerdaten": {
+                		"Adresse": "Dudelstr. 15, 12345 Dudelstadt",
+                		"Längengrad": 3,
+                		"Breitengrad": 5
+                	}
+                }
+                """, fakeAddressToGeoLocation, fakeLog);
+
+        assertEquals("Dudelstr. 15, 12345 Dudelstadt", config.Benutzerdaten.Adresse);
+        assertEquals(3, config.Benutzerdaten.Längengrad);
+        assertEquals(5, config.Benutzerdaten.Breitengrad);
+    }
+
+    @Test
+    public void updateWithUnknownEntryDoesNothing() {
+        SchiriConfiguration config = new SchiriConfiguration("");
+        config.updateWith("""
+                {
+                	"Spielablauf": {
+                		"Grillen": "gerne"
+                	}
+                }
+                """, fakeAddressToGeoLocation, fakeLog);
+        // Nothing happens
+    }
+
+    @Test
+    public void updateWithWrongTypeDoesNothing() {
+        SchiriConfiguration config = SchiriConfiguration.NEW_DEFAULT("");
+        int umziehenVorher = config.Spielablauf.UmziehenVorSpiel;
+        List<String> log = new ArrayList<>();
+        config.updateWith("""
+                {
+                	"Spielablauf": {
+                        "UmziehenVorSpiel": "Ein String!"
+                	}
+                }
+                """, fakeAddressToGeoLocation, fakeLog);
+
+        assertEquals(umziehenVorher, config.Spielablauf.UmziehenVorSpiel);
+    }
+    @Test
+    public void updateWithInvalidJSONCreatesLog() {
+        SchiriConfiguration config = SchiriConfiguration.NEW_DEFAULT("");
+        List<String> log = new ArrayList<>();
+        config.updateWith("""
+                {
+                	"Spielablauf": {
+                        "UmziehenVorSpiel": 25,
+                        "UmziehenNachSpiel
+                	
+                }
+                """, fakeAddressToGeoLocation, log::add);
+
+        assertEquals(1, log.size());
+    }
+
+    private Function<String, Optional<double[]>> fakeAddressToGeoLocation = (String) -> Optional.empty();
+    private Consumer<String> fakeLog = (String) -> {};
+
+
 }
