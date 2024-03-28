b8563ae8255b43b7e2a52db0e0d2cb2807e88e1d
Martin Dieblich <martin.dieblich@gmx.de>
2024-03-29 00:40:48 +0100

    added new Exceptions

---
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/MissingConfigException.java
@@ -0,0 +1,6 @@
+package com.dieblich.handball.schiedsrichterassistent;
+
+public class MissingConfigException extends Throwable {
+    public MissingConfigException(String s) {
+    }
+}
---
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/geo/GeoException.java
@@ -0,0 +1,6 @@
+package com.dieblich.handball.schiedsrichterassistent.geo;
+
+public class GeoException extends Throwable {
+    public GeoException(String s) {
+    }
+}
