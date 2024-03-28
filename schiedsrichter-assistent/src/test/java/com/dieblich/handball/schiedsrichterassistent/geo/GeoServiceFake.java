65b518927e807d6a679e269402cb264c51c782a9
Martin Dieblich <martin.dieblich@gmx.de>
2024-03-29 00:55:36 +0100

    removed unused method

--- schiedsrichter-assistent/src/test/java/com/dieblich/handball/schiedsrichterassistent/geo/GeoServiceFake.java
+++ schiedsrichter-assistent/src/test/java/com/dieblich/handball/schiedsrichterassistent/geo/GeoServiceFake.java
@@ -10,11 +10,6 @@ public class GeoServiceFake implements GeoService{
     private final Map<String, Koordinaten> koordinaten = new HashMap<>();
     private final Map<AbstractMap.SimpleImmutableEntry<Koordinaten, Koordinaten>, Fahrt> fahrten = new HashMap<>();
 
-    public void addFahrt(String startAddress, String endAddress, int dauerInSekunden, int distanzInMetern){
-        Koordinaten startCoords = addKoordinaten(startAddress);
-        Koordinaten endCoords = addKoordinaten(endAddress);
-        addFahrt(startCoords, endCoords, dauerInSekunden, distanzInMetern);
-    }
     public void addFahrt(Koordinaten startCoords, Koordinaten endCoords, int dauerInSekunden, int distanzInMetern){
         fahrten.put(pair(startCoords, endCoords), new Fahrt(dauerInSekunden, distanzInMetern));
     }
