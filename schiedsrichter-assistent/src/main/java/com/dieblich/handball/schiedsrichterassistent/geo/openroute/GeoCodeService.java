87e3394336650d55f75acdacb150fc04cfb1d86d
Martin Dieblich <martin.dieblich@gmx.de>
2024-04-21 21:02:03 +0200

    No need to URL-Encode the address
    Spring already does that!

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/geo/openroute/GeoCodeService.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/geo/openroute/GeoCodeService.java
@@ -3,8 +3,8 @@ package com.dieblich.handball.schiedsrichterassistent.geo.openroute;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Optional;
 
 public class GeoCodeService extends OpenRouteService{
@@ -17,16 +17,17 @@ public class GeoCodeService extends OpenRouteService{
             return Optional.empty();
         }
 
-        String encodedAddress = URLEncoder.encode(address, StandardCharsets.UTF_8);
-        // now a special hack: OpenRouteService somehow needs the "comma" unencoded, to correctly detect street + housenumber
-        encodedAddress = encodedAddress.replace("%2C", ",");
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(
+                MediaType.APPLICATION_JSON
+        ));
+        headers.setAcceptCharset(List.of(StandardCharsets.UTF_8));
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
 
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<GeoCode> response = restTemplate.exchange("https://api.openrouteservice.org/geocode/search?api_key="+getApikey()+"&text="+encodedAddress+"&size=1", HttpMethod.GET, requestEntity, GeoCode.class);
+        String fullURL = "https://api.openrouteservice.org/geocode/search?api_key="+getApikey()+"&text="+address+"&size=1";
+        ResponseEntity<GeoCode> response = restTemplate.exchange(fullURL, HttpMethod.GET, requestEntity, GeoCode.class);
 
         return Optional.ofNullable(response.getBody())
                 .flatMap(GeoCode::firstFeature)
