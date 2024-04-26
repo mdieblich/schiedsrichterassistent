cc910e217fc430ef0829ce662c19f1d012125c20
Martin Dieblich <martin.dieblich@gmx.de>
2024-04-27 00:49:46 +0200

    limitCellWidth

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/pdf/PDFTableCell.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/pdf/PDFTableCell.java
@@ -1,6 +1,8 @@
 package com.dieblich.handball.schiedsrichterassistent.pdf;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class PDFTableCell {
     public List<String> text;
@@ -26,6 +28,36 @@ public class PDFTableCell {
 
     public int rowspan() {
         return text.size();
+    }
+
+    public void limitCellWidth(double maxCellWidth, Function<String, Float> calculateStringWidth) {
+        List<String> newLines = new ArrayList<>();
+        for(String oldLine:text){
+            String[] words = oldLine.split(" ");
+            String currentLine = "";
+            for (String nextWord : words) {
+                if (calculateStringWidth.apply(currentLine + nextWord) > maxCellWidth) {
+                    if (!currentLine.isEmpty()) {
+                        newLines.add(currentLine.trim());
+                    }
+
+                    if (calculateStringWidth.apply(nextWord) > maxCellWidth) {
+                        // next word is already too long
+                        float actualWidth = calculateStringWidth.apply(nextWord);
+                        int splitPoint = (int) (nextWord.length() * maxCellWidth / actualWidth);
+                        newLines.add(nextWord.substring(0, splitPoint));
+                        currentLine = nextWord.substring(splitPoint) + " ";
+                    } else {
+                        currentLine = nextWord + " ";
+                    }
+
+                } else {
+                    currentLine += nextWord + " ";
+                }
+            }
+            newLines.add(currentLine.trim());
+        }
+        text = newLines;
     }
 
     public enum Alignment{
---
+++ schiedsrichter-assistent/src/test/java/com/dieblich/handball/schiedsrichterassistent/pdf/PDFTableCellTest.java
@@ -0,0 +1,61 @@
+package com.dieblich.handball.schiedsrichterassistent.pdf;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PDFTableCellTest {
+
+    @Test
+    public void limitCellWidth_NoLineBreak(){
+        PDFTableCell cell = new PDFTableCell("Markus und Moritz");
+        Function<String, Float> calculateStringWidth = text -> (float)text.length();
+
+        cell.limitCellWidth(20, calculateStringWidth);
+
+        assertEquals(List.of("Markus und Moritz"), cell.text);
+    }
+
+    @Test
+    public void limitCellWidth_OneLineBreak(){
+        PDFTableCell cell = new PDFTableCell("Markus und Moritz");
+        Function<String, Float> calculateStringWidth = text -> (float)text.length();
+
+        cell.limitCellWidth(10, calculateStringWidth);
+
+        assertEquals(List.of("Markus und", "Moritz"), cell.text);
+    }
+
+    @Test
+    public void limitCellWidth_TwoLineBreaks(){
+        PDFTableCell cell = new PDFTableCell("Markus und Moritz");
+        Function<String, Float> calculateStringWidth = text -> (float)text.length();
+
+        cell.limitCellWidth(7, calculateStringWidth);
+
+        assertEquals(List.of("Markus","und", "Moritz"), cell.text);
+    }
+
+    @Test
+    public void limitCellWidth_BreaksLongWord(){
+        PDFTableCell cell = new PDFTableCell("Heizungsmonteur");
+        Function<String, Float> calculateStringWidth = text -> (float)text.length();
+
+        cell.limitCellWidth(10, calculateStringWidth);
+
+        assertEquals(List.of("Heizungsmo","nteur"), cell.text);
+    }
+
+    @Test
+    public void limitCellWidth_BreaksLongWord_In_Between(){
+        PDFTableCell cell = new PDFTableCell("Martin und Heizungsmonteur Max");
+        Function<String, Float> calculateStringWidth = text -> (float)text.length();
+
+        cell.limitCellWidth(10, calculateStringWidth);
+
+        assertEquals(List.of("Martin und", "Heizungsmo","nteur Max"), cell.text);
+    }
+}
