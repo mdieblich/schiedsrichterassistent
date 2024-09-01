651fdc3dbec152b3e7ca12992933aa97872c6538
Martin Dieblich <martin.dieblich@gmx.de>
2024-09-01 22:39:37 +0200

    Extract builder class

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/received/AnsetzungsEmail.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/mail/received/AnsetzungsEmail.java
@@ -27,6 +27,57 @@ public class AnsetzungsEmail {
 
         List<SchiriEinsatz> einsaetze = new ArrayList<>();
 
+        SchiriEinsatzBuilder builder = new SchiriEinsatzBuilder();
+
+        String[] allLines = emailContent.split("\\r?\\n|\\r");
+        for(int i=0; i<allLines.length; i++){
+            String line = allLines[i].trim();
+            if(line.startsWith("Liga:")){
+                builder.liga = line.substring("Liga:".length()).trim();
+            } else if (line.startsWith("Ort:")){
+                String ortsangabe = line.substring("Ort:".length());
+                String[] ortsAngabenTeile = ortsangabe.split(", ");
+                if(ortsAngabenTeile.length >= 3) {
+                    builder.halleName = ortsAngabenTeile[0].trim();
+                    builder.halleStrasse = ortsAngabenTeile[1].trim();
+                    builder.hallePLZOrt = ortsAngabenTeile[2].trim();
+                }
+            } else if(line.startsWith("Spiel-Nr:")){
+                builder.spielNr = line.substring("Spiel-Nr:".length()).trim();
+                // Die Spiel-Angaben stehen in der nächsten Zeile
+                if(i<allLines.length-1){
+                    String nextLine = allLines[++i].trim();
+                    String anwurfString = nextLine.substring(0,16);
+                    builder.anwurf = LocalDateTime.parse(anwurfString, FORMATTER);
+                    String begegnung = nextLine.substring(16);
+                    String[] gegner = begegnung.split(" - ");
+                    if(gegner.length>=2){
+                        builder.heimMannschaft = gegner[0].trim();
+                        builder.gastMannschaft = gegner[1].trim();
+                    }
+                }
+            } else if(line.startsWith("SR-Gespann:")){
+                String gespann = line.substring("SR-Gespann:".length()).trim();
+                String[] schiris = gespann.split("/");
+                if(schiris.length >= 1){
+                    builder.schiriA = Schiedsrichter.fromNachnameVorname(schiris[0].trim());
+                }
+                if(schiris.length >= 2){
+                    builder.schiriB = Schiedsrichter.fromNachnameVorname(schiris[1].trim());
+                }
+            }
+
+            // now check if everything is complete, then we have a full Einsatz
+            if( builder.isComplete()){
+                einsaetze.add(builder.build());
+                builder.reset();
+            }
+        }
+
+        return einsaetze;
+    }
+
+    private static class SchiriEinsatzBuilder{
         String spielNr = null;
         LocalDateTime anwurf = null;
         String halleName = null;
@@ -39,78 +90,41 @@ public class AnsetzungsEmail {
         Schiedsrichter schiriA = null;
         Schiedsrichter schiriB = null;
 
-        String[] allLines = emailContent.split("\\r?\\n|\\r");
-        for(int i=0; i<allLines.length; i++){
-            String line = allLines[i].trim();
-            if(line.startsWith("Liga:")){
-                liga = line.substring("Liga:".length()).trim();
-            } else if (line.startsWith("Ort:")){
-                String ortsangabe = line.substring("Ort:".length());
-                String[] ortsAngabenTeile = ortsangabe.split(", ");
-                if(ortsAngabenTeile.length >= 3) {
-                    halleName = ortsAngabenTeile[0].trim();
-                    halleStrasse = ortsAngabenTeile[1].trim();
-                    hallePLZOrt = ortsAngabenTeile[2].trim();
-                }
-            } else if(line.startsWith("Spiel-Nr:")){
-                spielNr = line.substring("Spiel-Nr:".length()).trim();
-                // Die Spiel-Angaben stehen in der nächsten Zeile
-                if(i<allLines.length-1){
-                    String nextLine = allLines[++i].trim();
-                    String anwurfString = nextLine.substring(0,16);
-                    anwurf = LocalDateTime.parse(anwurfString, FORMATTER);
-                    String begegnung = nextLine.substring(16);
-                    String[] gegner = begegnung.split(" - ");
-                    if(gegner.length>=2){
-                        heimMannschaft = gegner[0].trim();
-                        gastMannschaft = gegner[1].trim();
-                    }
-                }
-            } else if(line.startsWith("SR-Gespann:")){
-                String gespann = line.substring("SR-Gespann:".length()).trim();
-                String[] schiris = gespann.split("/");
-                if(schiris.length >= 1){
-                    schiriA = Schiedsrichter.fromNachnameVorname(schiris[0].trim());
-                }
-                if(schiris.length >= 2){
-                    schiriB = Schiedsrichter.fromNachnameVorname(schiris[1].trim());
-                }
-            }
-
-            // now check if everything is complete, then we have a full Einsatz
-            if( spielNr        != null &&
-                anwurf         != null &&
-                halleName      != null &&
-                halleStrasse   != null &&
-                hallePLZOrt    != null &&
-                liga           != null &&
-                heimMannschaft != null &&
-                gastMannschaft != null &&
-                schiriA        != null){ // no need to check schiriB. Either both are set or none
-
-                einsaetze.add(new SchiriEinsatz(
-                        spielNr,
-                        anwurf,
-                        halleName, halleStrasse, hallePLZOrt,
-                        liga, heimMannschaft, gastMannschaft,
-                        schiriA, schiriB
-                ));
-                // reset all buffers
-                spielNr = null;
-                anwurf = null;
-                halleName = null;
-                halleStrasse = null;
-                hallePLZOrt = null;
-                liga = null;
-                heimMannschaft = null;
-                gastMannschaft = null;
-
-                schiriA = null;
-                schiriB = null;
-            }
+        public boolean isComplete(){
+            return  spielNr        != null &&
+                    anwurf         != null &&
+                    halleName      != null &&
+                    halleStrasse   != null &&
+                    hallePLZOrt    != null &&
+                    liga           != null &&
+                    heimMannschaft != null &&
+                    gastMannschaft != null &&
+                    schiriA        != null;
+            // no need to check schiriB. Either both are set or none
         }
 
-        return einsaetze;
+        public SchiriEinsatz build(){
+            return new SchiriEinsatz(
+                    spielNr,
+                    anwurf,
+                    halleName, halleStrasse, hallePLZOrt,
+                    liga, heimMannschaft, gastMannschaft,
+                    schiriA, schiriB
+            );
+        }
+        public void reset(){
+            spielNr = null;
+            anwurf = null;
+            halleName = null;
+            halleStrasse = null;
+            hallePLZOrt = null;
+            liga = null;
+            heimMannschaft = null;
+            gastMannschaft = null;
+
+            schiriA = null;
+            schiriB = null;
+        }
     }
 
 }
