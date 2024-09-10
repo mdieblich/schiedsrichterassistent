4518ad05532a55799fc190edd1ebeac9ff95e414
Martin Dieblich <martin.dieblich@gmx.de>
2024-09-10 22:13:07 +0200

    NULL-Fahrt

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/geo/Fahrt.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/geo/Fahrt.java
@@ -6,6 +6,8 @@ public record Fahrt (
    int dauerInMinuten,
    int distanzInKilometern
 ){
+    public static final Fahrt NULL = new Fahrt(0,0);
+
     public Fahrt(Segment segment){
         this((int)Math.ceil(segment.duration()/60), (int)Math.round(segment.distance()/1000));
     }
