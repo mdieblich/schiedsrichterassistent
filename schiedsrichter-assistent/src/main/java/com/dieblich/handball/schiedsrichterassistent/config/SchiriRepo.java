a079486d6d781ae79e4e3d0b4fd80945aed176d2
Martin Dieblich <martin.dieblich@gmx.de>
2024-11-17 18:45:30 +0100

    Fetch Schiris inside of SchiriRepo

--- schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/config/SchiriRepo.java
+++ schiedsrichter-assistent/src/main/java/com/dieblich/handball/schiedsrichterassistent/config/SchiriRepo.java
@@ -1,10 +1,14 @@
 package com.dieblich.handball.schiedsrichterassistent.config;
 
 import com.dieblich.handball.schiedsrichterassistent.Schiedsrichter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Optional;
+import java.util.*;
 
 public interface SchiriRepo {
+
+    Logger logger = LoggerFactory.getLogger(SchiriRepo.class);
 
     Optional<SchiriConfiguration> findConfigByEmail(String emailAddress) throws SchiriRepoException;
 
@@ -17,4 +21,46 @@ public interface SchiriRepo {
         }
     }
 
+    default ConfiguredSchiris fetchSchiris(Collection<String> emailAddresses){
+        logger.info("Fetching {} schiris", emailAddresses.size());
+        ConfiguredSchiris configuredSchiris = new ConfiguredSchiris();
+
+        for(String knownSchiri:emailAddresses){
+            try {
+                Optional<SchiriConfiguration> optionalSchiriConfig = findConfigByEmail(knownSchiri);
+                if (optionalSchiriConfig.isPresent() && optionalSchiriConfig.get().isComplete()) {
+                    logger.info("Full config found for {}", knownSchiri);
+                    configuredSchiris.fullyConfiguredSchiris.put(knownSchiri, optionalSchiriConfig.get());
+                } else {
+                    logger.info("Partly config found for {}", knownSchiri);
+                    configuredSchiris.addPartlyConfiguredSchiri(knownSchiri);
+                }
+            } catch (SchiriRepo.SchiriRepoException e) {
+                logger.warn("Exception occurred for {}:", knownSchiri, e);
+                configuredSchiris.addException(knownSchiri, e);
+            }
+        }
+
+        logger.info(
+                "Finished fetching schiris: {} were fully and {} partly configured.",
+                configuredSchiris.fullyConfiguredSchiris.size(),
+                configuredSchiris.partlyConfiguredSchiris.size()
+        );
+
+        return configuredSchiris;
+    }
+
+    class ConfiguredSchiris{
+        public Map<String, SchiriConfiguration> fullyConfiguredSchiris = new HashMap<>();
+        public Map<String, List<Exception>> partlyConfiguredSchiris = new HashMap<>();
+        public void addPartlyConfiguredSchiri(String emailAddress){
+            if(!partlyConfiguredSchiris.containsKey(emailAddress)){
+                partlyConfiguredSchiris.put(emailAddress, new ArrayList<>());
+            }
+        }
+        public void addException(String emailAddress, Exception e){
+            addPartlyConfiguredSchiri(emailAddress);
+            partlyConfiguredSchiris.get(emailAddress).add(e);
+        }
+    }
 }
